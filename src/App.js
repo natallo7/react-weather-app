bfa4bb971ce82754f2338f3a385ad7fea9e9e5c2
natallo7 <102995207+natallo7@users.noreply.github.com>
2022-10-04 13:50:57 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      <h1>Weather Application</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://natalialloyd.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Natalia Lloyd{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/natallo7/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
