c6217c891542741ee21d6c6ffc193a0e09257cae
natallo7 <102995207+natallo7@users.noreply.github.com>
2022-10-10 17:02:45 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather Application</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://natalialloyd.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Natalia Lloyd{" "}
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/natallo7/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather Application</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://natalialloyd.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Natalia Lloyd{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/natallo7/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
